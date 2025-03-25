a4bb154bd322e4d29532b7c5d063fb005005519c
João Vitor <joaovitor.david.silva@gmail.com>
2025-03-24 21:43:19 -0300

    removido contato e sobre

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,6 +1,6 @@
 
 from django.urls import path
-from recipes.views import home,sobre,contato
+from recipes.views import home
 
 
 
@@ -8,6 +8,5 @@ from recipes.views import home,sobre,contato
 urlpatterns = [
   
     path('',home ), # home
-    path('sobre/',sobre ),#/sobre
-    path('contato/',contato),# contato
+  
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.shortcuts import render
 #HTTP request e o servidor retorna para ele uma HTTP response
 
 def home(request):
@@ -8,11 +6,5 @@ def home(request):
         'nome': 'joão',
     })
 
-
-def sobre(request):
-    return HttpResponse('sobre')
-
-def contato(request):
-    return HttpResponse('contato')
 
 # Create your views here.
